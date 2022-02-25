bdd3044aae15ca7caa341ea98b741add75bb12b6
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2022-02-24 19:27:41 -0500

    add solution for 03-OO-ruby-pt2

--- lectures/03-OO-ruby-pt2/lib/Patient.rb
+++ lectures/03-OO-ruby-pt2/lib/Patient.rb
@@ -1,33 +1,54 @@
 class Patient
-    attr_reader :id
-    attr_accessor :species, :age, :name, :owner, :phone
+    attr_accessor :id, :species, :age, :name, :owner, :phone
 
+    # Patient.new(params)
     @@all = []
 
-    # Public / Private Method?
-    def initialize(species, age, name, owner, phone)
-        @id  = @@all.length + 1
-        @species = species
-        @age = age
-        @name = name
-        @owner = owner
-        @phone = phone
-
-        binding.pry
+    def initialize(attributes)
+        attributes.each do |key, value| 
+          self.send("#{key}=", value)
+        end
 
         add_self
     end
 
-    # Class method (Patient.all)
-    # self => Patient class
-    def self.all
-        @@all
-    end
+    # Class Methods
 
-    # Instance method (new_patient.give_name)
-    def give_name
-        @name
-    end
+        # Patient.all
+        # self => Patient class
+        def self.all
+            @@all
+        end
+
+        def self.all_species
+            self.all.map { |p| p.species }.uniq
+            # @@all.map { |p| p.species }.uniq
+        end
+
+        # { species:"dog", age: "2", name:"Jack", owner:"John Smith", phone: "999-999-9999"},
+        def self.find_patient(name, owner)
+            # self.all
+            # find => Returns the first matching instance
+            patient = @@all.find { |p| p.name == name && p.owner == owner }
+            
+            if patient 
+                patient.name 
+            end
+        end
+
+    # Instance Methods
+
+        # new_patient.give_name
+        def give_name
+            @name
+        end 
+
+        def delete_patient
+            # binding.pry
+           
+            @@all = @@all.filter { |p| p != self }
+           puts "#{self.name} was removed from the system."
+        end
 
     private
 
@@ -36,3 +57,10 @@ class Patient
         @@all << self
     end
 end
+
+class Cat < Patient
+    # Inherits instance / class methods, etc. 
+end
+
+# new_cat = Cat.new
+# new_cat.give_name => "Milo"
--- lectures/03-OO-ruby-pt2/lib/cli.rb
+++ lectures/03-OO-ruby-pt2/lib/cli.rb
@@ -1,8 +1,10 @@
-# $patients = [
-#     { species:"dog", age: "2", name:"Jack", owner:"John Smith", phone: "999-999-9999"},
-#     { species:"bird", age: "3", name:"Mia", owner:"Jane Doe", phone: "888-888-8888"},
-#     { species:"cat", age: "4", name:"Grace", owner:"Louis", phone: "777-777-7777"},
-# ]
+$patients = [
+    { species:"dog", age: "2", name:"Jack", owner:"John Smith", phone: "999-999-9999"},
+    { species:"bird", age: "3", name:"Mia", owner:"Jane Doe", phone: "888-888-8888"},
+    { species:"cat", age: "4", name:"Grace", owner:"Louis", phone: "777-777-7777"},
+    # duplicate record 
+    { species:"cat", age: "4", name:"Grace", owner:"Louis", phone: "777-777-7777"},
+]
 
 $clinics = [
     { clinic_name: "Flatiron Veterinary Clinic", address: "111 Hacker Way", phone: "777-777-7777"},
@@ -11,10 +13,41 @@ $clinics = [
 ]
 
 def initialize_app
+    seed
     menu
 end
 
+def try_again
+    puts "Sorry, that option doesn't exist. Please try again!"
+    menu
+end
+
+def return_to_menu
+    puts "Press any key to return"
+    gets.strip
+    menu
+end
+
+def search
+    puts "Enter a patient name:"
+    name = gets.strip
+    puts "Enter a patient owner:"
+    owner = gets.strip
+
+    
+    if Patient.find_patient(name, owner)
+        puts "Patient Found: #{Patient.find_patient(name, owner)}"
+    else
+        puts "No Patient Found"
+    end
+end
+
+def is_found(id)
+    Patient.all.find { |p| p.id.to_s == id}
+end
+
 def menu
+    puts "_______________________________________________________"
     puts "\n" + "Welcome to Flatiron Veterinary Clinic!" + "\n\n"
     puts "Please choose an option:" ++ "\n\n"
     puts "1. List All Patients"
@@ -22,6 +55,8 @@ def menu
     puts "3. List Patient Species"
     puts "4. List Clinic Names"
     puts "5. Add New Patient"
+    puts "6. Find Patient"
+    puts "7. Delete Patient"
     puts "0. Exit" + "\n\n"
 
     # initial user input prompt
@@ -29,58 +64,85 @@ def menu
     user_input = gets.strip
 
     case user_input
-    
+
+    when "7"
+        puts "Please enter an ID"
+        
+        # get ID from user
+        id = gets.strip 
+
+        # find a Patient by id
+        
+        # binding.pry
+
+        if is_found(id)
+            patient = Patient.all.find { |p| p.id.to_s == id }
+            patient.delete_patient
+        else
+            puts "No Patient Found"
+        end
+        return_to_menu
+    when "6"
+        search
+        return_to_menu
     when "5"
         create_patient
-        menu
+        return_to_menu
     when "4"
-        # .map returns an Array
-        clinics_array = $clinics.map{|clinic| clinic[:clinic_name]}
+        clinics_array = $clinics.map{|clinic| 
+            "Clinic Name: #{clinic[:clinic_name]}"
+        }
+
         puts clinics_array
-        
-        # recursive method call
-        menu
+
+        return_to_menu
     when "3"
         # .map returns an Array
-        patients_array = Patient.all.map{|patient| patient[:species]}
-        puts patients_array
-        menu
+        puts Patient.all_species
+        return_to_menu
     when "2"
-        $clinics.each { |clinic| puts clinic }
-        menu
+        $clinics.each { |clinic| 
+            puts "Clinic Hash Object: #{clinic}"
+        }
+        return_to_menu
     when "1"
         if Patient.all.length != 0
             puts "\n"
             Patient.all.each { |patient| 
-                puts "-----------------------"
+                puts "_______________________"
                 puts "ID: #{patient.id}" 
                 puts "Species: #{patient.species}" 
                 puts "Age: #{patient.age}"
                 puts "Name: #{patient.name}"
                 puts "Owner: #{patient.owner}"
                 puts "Phone: #{patient.phone}"
-                puts "-----------------------"
+                puts "_______________________"
             }
+
+            return_to_menu
         else
             puts "\n" + "No Patients!"
+            return_to_menu
         end
-        menu      
+        # menu      
     when "0"
         puts "Goodbye!"
+    else
+        try_again
     end
 end
     
 def create_patient
     puts "Please enter a patient species"
-    new_species = gets.strip
+    new_species = gets.strip!
     puts "Please enter a patient age"
-    new_age = gets.strip
+    new_age = gets.strip!
     puts "Please enter a patient name"
-    new_name = gets.strip
+    new_name = gets.strip!
     puts "Please enter a patient owner"
-    new_owner = gets.strip
+    new_owner = gets.strip!
     puts "Please enter a patient phone"
-    new_phone = gets.strip
+    new_phone = gets.strip!
 
     # create hash literal for each new_patient
     # new_patient = {
@@ -92,13 +154,25 @@ def create_patient
     # }
 
     # species, age, name, owner, phone
-    p1 = Patient.new(
-        new_species, 
-        new_age, 
-        new_name,
-        new_owner,
-        new_phone
-    );
+    p1 = Patient.new({
+        id: Patient.all.length + 1,
+        species: new_species, 
+        age: new_age, 
+        name: new_name,
+        owner: new_owner,
+        phone: new_phone
+    });
+end
+
+def seed
+    $patients.each { |p| Patient.new({
+        id: Patient.all.length + 1,
+        species: p[:species],
+        age: p[:age],
+        name: p[:name],
+        owner: p[:owner],
+        phone: p[:phone]
+    })}
 end
 
 # New Patient Logic (End)
