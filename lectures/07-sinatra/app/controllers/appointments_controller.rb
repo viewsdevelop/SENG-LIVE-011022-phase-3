067af952fe0ee531a15adf3f10b5aef206313c74
Louis Michael Medina <louis@Louis-Medinas-MacBook-Pro.local>
2022-03-04 13:26:07 -0500

    add starter code for 07-sinatra

---
+++ lectures/07-sinatra/app/controllers/appointments_controller.rb
@@ -0,0 +1,13 @@
+class AppointmentsController < ApplicationController
+    
+    # Activity 1 => Implementing Routes in Sinatra
+
+    # NOTE => Run `bundle exec rake server` to view JSON responses in the Browser
+
+    # /appointments => Retrieve All Appointments
+    # BONUS => Include Only Appointment Date + Associated Vet / Patient Names
+    # BONUS => Set Limit of First 50 Appointments Ordered By Date
+
+    # /appointments/:id => Retrieve an Individual Appointment via Params (:id)
+    
+end 
---
+++ lectures/07-sinatra/app/controllers/vets_controller.rb
@@ -0,0 +1,13 @@
+class VetsController < ApplicationController
+    
+    # Activity 2 => Implementing Routes in Sinatra
+
+    # NOTE => Run `bundle exec rake server` to view JSON responses in the Browser
+
+    # /vets => Retrieve All Vets and Associated Patients
+    # BONUS => Include Only Vet's Name & Speciality Attributes
+
+    # /vets/:id => Retrieve an Individual Vet via Params (:id)
+    # BONUS => Include Vet's Associated Appointments
+    
+end 
